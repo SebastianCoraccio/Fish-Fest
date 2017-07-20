44299313238d86feb862e2d4767df0694a72c5bc
Sebastian Coraccio <sry84@wildcats.unh.edu>
2017-07-19 21:19:20 -0400

    Added greyed out chum on store page

--- scenes/store.lua
+++ scenes/store.lua
@@ -110,6 +110,8 @@ local function changeBait()
   baitTitleText.text = baitInfo[selectedBait].name .. " x" .. db:getRows("StoreItems")[1][baitInfo[selectedBait].dbName]
 
   -- Set big picture image
+  bigPicture = display.newImage("images/baits/" .. string.lower(baitInfo[selectedBait].name) .."_large.png",  175, 1000)
+  baitGroup:insert(bigPicture)
 
   -- Check if the buy button needs to be changed
   buyButton:setLabel("Buy for " .. baitInfo[selectedBait].cost)
@@ -385,8 +387,8 @@ function scene:create(event)
 
   -- Set up selected bait area
   -- big picture
-	bigPicture = display.newImage("images/baits/chum_large.png", 175, 1000)
-	-- bigPicture = display.newRoundedRect(-150, -200, display.contentWidth / 3, display.contentHeight / 3, 12)
+	bigPicture = display.newImage("images/baits/" .. string.lower(baitInfo[selectedBait].name) .."_large.png",  175, 1000)
+    -- bigPicture = display.newRoundedRect(-150, -200, display.contentWidth / 3, display.contentHeight / 3, 12)
 	-- bigPicture:setFillColor(0)
 	-- bigPicture:setStrokeColor(78, 179, 211)
 	-- bigPicture.strokeWidth = 4
