db6710555140d690ca8d979c29ec44e12ef4adaf
Sebastian Coraccio <sry84@wildcats.unh.edu>
2017-07-19 22:10:17 -0400

    Better Rod descriptions

--- data/rodInfo.lua
+++ data/rodInfo.lua
@@ -2,11 +2,11 @@
 -- Holds all information about the different rods
 return {
   {name="Splintered Rod",       image="", cost=0,       description="A decrepit rod you found under a dock. It feels like it might disintegrate in your hands at any moment"},
-  {name="The David Rod",        image="", cost=10000,   description="Not pretty, but it works"},
-  {name="The Sebastian Rod",    image="", cost=75000,   description="Very Pretty, and works great!"},
-  {name="Rod 4",                image="", cost=150000,  description="Its a rod"},
-  {name="Rod 5",                image="", cost=250000,  description="Another one"},
-  {name="Rod 6",                image="", cost=500000,  description=":steve:"},
-  {name="Rod 7",                image="", cost=750000,  description=":sven:"},
+  {name="Bamboo Rod",           image="", cost=10000,   description="A relic of an earlier generation of angling. It may not beat out the new rods, but it still gets the job done."},
+  {name="Fiberglass Rod",       image="", cost=75000,   description="A modern tool to catch a wide range of fish in different locations."},
+  {name="Still Playing?",       image="", cost=150000,  description="Honestly, we didn't expect anyone to play enough of the alpah to get this..."},
+  {name="Rod 5",                image="", cost=250000,  description="5"},
+  {name="Rod 6",                image="", cost=500000,  description="6"},
+  {name="Rod 7",                image="", cost=750000,  description="7"},
   {name="The Golden Rod",       image="", cost=999999,  description="The mark of a master angler. Fish yearn to be caught with this rod"}
 }
