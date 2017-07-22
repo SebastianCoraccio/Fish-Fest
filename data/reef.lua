cbfe1929894ceaf8827323c294ba2fc3d2a7847f
Sebastian Coraccio <sry84@wildcats.unh.edu>
2017-07-22 15:55:40 -0400

    Added reef location

---
+++ data/reef.lua
@@ -0,0 +1,24 @@
+-- reef
+-- Return table containing fish information for reef location
+
+return {
+  fish = {
+    {fid=23, spawnChance=1},
+    {fid=48, spawnChance=1},
+    {fid=50, spawnChance=1},
+    {fid=49, spawnChance=2},
+    {fid=41, spawnChance=3},
+    {fid=44, spawnChance=4},
+    {fid=37, spawnChance=5},
+    {fid=36, spawnChance=5},
+    {fid=42, spawnChance=6},
+    {fid=45, spawnChance=7},
+    {fid=40, spawnChance=7},
+    {fid=35, spawnChance=7},
+    {fid=47, spawnChance=8},
+    {fid=38, spawnChance=8},
+    {fid=46, spawnChance=11},
+    {fid=43, spawnChance=12},
+    {fid=39, spawnChance=12}
+  }
+}
