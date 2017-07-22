c0cc5db726370f6b5120e04bae06cfdc0282c559
Sebastian Coraccio <sry84@wildcats.unh.edu>
2017-07-22 11:27:09 -0400

    Ice Caps location

---
+++ data/icecap.lua
@@ -0,0 +1,18 @@
+-- icecap
+-- Return table containing fish information for ice cap location
+
+return {
+  fish = {
+    {fid=23, spawnChance=0.5},
+    {fid=33, spawnChance=1},
+    {fid=32, spawnChance=2},
+    {fid=31, spawnChance=4},
+    {fid=29, spawnChance=6},
+    {fid=30, spawnChance=10},
+    {fid=25, spawnChance=10},
+    {fid=27, spawnChance=13},
+    {fid=26, spawnChance=15},
+    {fid=28, spawnChance=18.5},
+    {fid=24, spawnChance=20}
+  }
+}
