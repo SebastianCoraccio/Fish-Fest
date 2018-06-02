d1e22bc3bf42ffe476ec0c4a72dc656a1a8d1097
Sebastian Coraccio <sebastiancoraccio@gmail.com>
2018-06-02 19:22:45 -0400

    Updated levelup values

--- data/levelInfo.lua
+++ data/levelInfo.lua
@@ -1,12 +1,12 @@
 -- rodInfo
 -- Holds all information about the different rods
 return {
-  {name="Splintered Rod",       image="rod.png",             cost=500,    description="A decrepit rod you found under a dock. It feels like it might disintegrate in your hands at any moment"},
-  {name="Bamboo Rod",           image="bamboo_rod.png",      cost=10000,   description="A relic of an earlier generation of angling. It may not beat out newer rods, but it still gets the job done."},
-  {name="Fiberglass Rod",       image="fiber_glass_rod.png", cost=25000,   description="A modern tool to catch a wide range of fish in different locations."},
-  {name="Still Playing?",       image="rod.png",             cost=150000,  description="Honestly, we didn't expect anyone to play enough of the alpah to get this..."},
-  {name="Rod 5",                image="rod.png",             cost=250000,  description="5"},
-  {name="Rod 6",                image="rod.png",             cost=500000,  description="6"},
-  {name="Rod 7",                image="rod.png",             cost=750000,  description="7"},
-  {name="The Golden Rod",       image="rod.png",             cost=999999,  description="The mark of a master angler. Fish yearn to be caught with this rod"}
+  {name="Splintered Rod",       image="rod.png",             cost=2500,    description="A decrepit rod you found under a dock. It feels like it might disintegrate in your hands at any moment"},
+  {name="Bamboo Rod",           image="bamboo_rod.png",      cost=5000,   description="A relic of an earlier generation of angling. It may not beat out newer rods, but it still gets the job done."},
+  {name="Fiberglass Rod",       image="fiber_glass_rod.png", cost=10000,   description="A modern tool to catch a wide range of fish in different locations."},
+  {name="Still Playing?",       image="rod.png",             cost=25000,  description="Honestly, we didn't expect anyone to play enough of the alpah to get this..."},
+  {name="Rod 5",                image="rod.png",             cost=50000,  description="5"},
+  {name="Rod 6",                image="rod.png",             cost=100000,  description="6"},
+  {name="Rod 7",                image="rod.png",             cost=250000,  description="7"},
+  {name="The Golden Rod",       image="rod.png",             cost=500000,  description="The mark of a master angler. Fish yearn to be caught with this rod"}
 }
